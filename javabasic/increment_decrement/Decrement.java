9829446ebb4982629c6418f1417e0d3dc114f3ab
Manish10022001 <manishshirsat2850@gmail.com>
2025-07-27 13:30:49 +0530

    Increment / Decrement

---
+++ javabasic/increment_decrement/Decrement.java
@@ -0,0 +1,12 @@
+package javabasic.increment_decrement;
+
+public class Decrement {
+    public static void main(String args[]){
+        int i = 0;
+        //post decrement
+        System.out.println("Post decrement: "+ i--);
+
+        //pre decrement
+        System.out.println("Pre decrement: "+ --i);
+    }
+}
---
+++ javabasic/increment_decrement/Increment.java
@@ -0,0 +1,13 @@
+package javabasic.increment_decrement;
+
+public class Increment {
+    public static void main(String args[]){
+        int i = 0;
+        //post increment
+        System.out.println("after post increemnt: "+ i++);//her 1st value will be 0 after printing 1st value i.e 0 , it will increment. 
+
+        
+        //pre increment
+        System.out.println("Pre increment: "+ ++i); //value is first incremented and then printed
+    }
+}
