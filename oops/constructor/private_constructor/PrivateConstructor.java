f34d86b3a730eebdb18dbc308b2bb24ae2e3f467
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-21 18:38:00 +0530

    Private Constructor

--- oops/constructor/private_constructor/PrivateConstructor.java
+++ oops/constructor/private_constructor/PrivateConstructor.java
@@ -1,20 +1,27 @@
 package oops.constructor.private_constructor;
 
 class Singleton{
+    //Create a private static instance of the class
+    private static Singleton instance;
+
+    //Private constructor prevents instantiation from other classes
     private Singleton(){
         System.out.println("In private constructor");
     }
 
-    public static Singleton getObject(){
+    //Public static method to provide access to the instance
+    public static Singleton getInstance(){
         if(instance == null){
             instance = new Singleton();
         }
         return instance;
     }
-    private static Singleton instance;
+    
 }
 public class PrivateConstructor {
     public static void main(String args[]){
-        Singleton inst = Singleton.getObject();
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1==s2);
     }    
 }
