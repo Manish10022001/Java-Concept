9ab7c81f66864f4904c3cf8b5746fbe7cc816de8
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-29 23:16:28 +0530

    string method: equals(), contains(), toUpperCase, toLowerCase

--- strings/TwoStringMethods.java
+++ strings/TwoStringMethods.java
@@ -2,7 +2,7 @@ package strings;
 public class TwoStringMethods{
     public static void main(String args[]){
         String str = "Hello Java";
-
+        String string = "Bye Java";
         //length()
         System.out.println(str.length());
 
@@ -12,5 +12,17 @@ public class TwoStringMethods{
         //substring(start,end)
         System.out.println(str.substring(2,4));
         System.out.println(str.substring(3));
+
+        //equals()
+        System.out.println(str.equals(string));
+
+        //contains()
+        System.out.println(str.contains("ello"));
+
+        //toUpperCase and toLowerCase
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+        
     }
 }
