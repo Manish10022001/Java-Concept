4a341ec4f0b0886eb67ef268c8345fb36ac1719a
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-29 23:07:39 +0530

    length string method

---
+++ strings/TwoStringMethods.java
@@ -0,0 +1,9 @@
+package strings;
+public class TwoStringMethods{
+    public static void main(String args[]){
+        String str = "Hello Java";
+
+        //length()
+        System.out.println(str.length());
+    }
+}
