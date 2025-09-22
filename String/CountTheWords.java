8da39f0bbe687b231ea4e74f94fcc1893706f77e
Manish10022001 <manishshirsat2850@gmail.com>
2025-09-22 20:17:17 +0530

    count the words in string

---
+++ String/CountTheWords.java
@@ -0,0 +1,15 @@
+package String;
+import java.util.Scanner;
+
+//Write a Java program to count the number of words in a string?
+public class CountTheWords {
+    public static void main(String args[]){
+        System.out.println("Enter the string: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String[] words = s.trim().split(" ");
+        System.out.println("Number of words in the string are : "+ words.length);
+        sc.close();
+    }
+}
